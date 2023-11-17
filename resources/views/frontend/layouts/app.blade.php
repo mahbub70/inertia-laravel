60c7fce312b0acec6d80e0c281691689b25e4c8e
mahbub <www.rokon7070@gmail.com>
2023-11-18 01:27:03 +0600

    First Init

---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Frontend Layouts</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    </head>
+    <body>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel consequuntur nesciunt aspernatur. Pariatur dolores aspernatur tempora dolorum explicabo rerum quod, commodi voluptas esse quas magnam impedit aliquid perferendis dicta inventore!
+    </body>
+</html>
